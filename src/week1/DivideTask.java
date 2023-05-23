97225fd1db024ed9818025968723a17ba597ae80
Branko <banebumbar88@hotmail.com>
2023-05-23 12:42:43 -0400

    Divide task updated

--- src/week1/DivideTask.java
+++ src/week1/DivideTask.java
@@ -1,10 +1,9 @@
 package week1;
-
 public class DivideTask {
-    public static void main(String[] args) {
-        divide(100, 4);
 
-    }
+public static void main(String[]args){
+        divide(100,4);
+        }
     public static void divide (int a, int b) {
         System.out.println(a / b);
 
