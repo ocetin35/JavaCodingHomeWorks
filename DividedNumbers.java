13a93514eee10670ab0ee1cb8f5769bfa708f8b9
Emre Durmaz <emredrmz@gmail.com>
2023-05-24 21:23:07 -0400

    Revert "DividedNumbers Updated..."
    This reverts commit bd69dede9e7ba38843314c4fcc68950828bf60b6.

--- DividedNumbers.java
+++ DividedNumbers.java
@@ -1,34 +1,6 @@
 public class DividedNumbers {
 
-    public static void main(String[] args) {
-
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Please enter dividend number");
-        int dividend = scan.nextInt();
-
-        System.out.println("Please enter your divisor number");
-        int divisor = scan.nextInt();
-
-        System.out.println(divide(dividend, divisor));
-
-    }
-    public static int divide(int dividend, int divisor) {
-
-        int quotient = 0;
-
-        while (dividend >= divisor) {
-            dividend -= divisor;
-            quotient++;
-        }
-
-        return quotient;
-    }
-}
-
-
-
-
-
+ // updating...
 
 }
 
