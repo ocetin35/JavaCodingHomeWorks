8a1d5e64c2ddc1bc5135632869dae48b41397124
Ozlem <ozlembulgak35@gmail.com>
2023-05-25 20:34:40 -0400

    classes file

---
+++ src/week1/Q2.java
@@ -0,0 +1,27 @@
+package week1;
+
+public class Q2 {
+    /*
+Divide without / operator:
+        Write a method that can divide two numbers without using division operator
+ */
+
+    public static void division(int dividing, int diveder) {
+
+        int count = 0;
+
+        while (dividing >= diveder) {
+
+            dividing -= diveder;
+
+            count++;
+
+
+        }
+
+        System.out.println("result : " + count + "\nremainder : " + dividing);
+
+    }
+
+
+}
