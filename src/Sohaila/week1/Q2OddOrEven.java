efff32f405946e49eabfaa939bf56cb25b9841a2
Sohaila_Cydeo <Ksohaila20@gmail.com>
2023-05-31 17:09:16 -0400

    added comments to Q2FOddOrEven.java

--- src/Sohaila/week1/Q2OddOrEven.java
+++ src/Sohaila/week1/Q2OddOrEven.java
@@ -9,9 +9,9 @@ public class Q2OddOrEven {
         System.out.println("Enter a number:");
         int number = input.nextInt();
         
-        if (number % 2 == 0) {
+        if (number % 2 == 0) { // if a number is divisible by two equals zero its even
             System.out.println(number + " is even");
-        } else if (number % 2 != 0) {
+        } else if (number % 2 != 0) {// if a number is divisible by two is not equals zero it's odd
             System.out.println(number + " is odd");
         }
 
