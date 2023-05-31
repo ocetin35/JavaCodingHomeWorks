6553d1015b1086753165c4b759670715e2bb8ebc
Sohaila_Cydeo <Ksohaila20@gmail.com>
2023-05-31 15:24:00 -0400

    replied to Q1

--- src/Sohaila/week1/Q1.java
+++ src/Sohaila/week1/Q1.java
@@ -1,4 +1,29 @@
 package Sohaila.week1;
 
 public class Q1 {
+        public static void main(String[] args) {
+            int a = 60;
+            int b = 8;
+
+            int count = 0;
+            while (a >= b) {
+                a -= b;
+                count++;
+
+            }
+            System.out.println(count + " with a reminder of " + a);
+
+            System.out.println("_______________________________________________________________");
+
+            int dividend = 60;
+            int divisor = 5;
+            int quotient = 0;
+
+            while (dividend >= divisor) {
+                dividend -= divisor;
+                quotient++;
+            }
+            System.out.println("Quotient = " + quotient);
+
+        }
 }
