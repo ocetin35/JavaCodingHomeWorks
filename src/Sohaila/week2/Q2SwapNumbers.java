c28ca747f0fa8c650ab0450794eb60ec2efb8e54
Sohaila_Cydeo <Ksohaila20@gmail.com>
2023-06-01 18:25:25 -0400

    add comment

--- src/Sohaila/week2/Q2SwapNumbers.java
+++ src/Sohaila/week2/Q2SwapNumbers.java
@@ -2,6 +2,9 @@ package Sohaila.week2;
 
 public class Q2SwapNumbers {
 
+    /*
+    Swap two variable without using a third variable.
+     */
     public static void main(String[] args) {
         int a =10;
         int b = 15;
