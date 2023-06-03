6e869f437e163fbe170fd6a37b8e4dd15a7fb3dc
Moses <gichchege@gmail.com>
2023-06-02 20:40:36 -0700

    Odd or Even Created

---
+++ src/Moses/week1/OddOrEven.java
@@ -0,0 +1,20 @@
+package Moses.week1;
+
+public class OddOrEven {
+
+    public static void main(String[] args) {
+
+        int num1 = 6;
+        int num2 = 5;
+
+        if(num1 % 2 == 0){
+            System.out.println("Number is even");
+        }else{
+            System.out.println("number is odd");
+        }
+
+    }
+
+
+}
+
