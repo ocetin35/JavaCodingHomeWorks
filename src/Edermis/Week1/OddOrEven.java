1b67440793c77dd5f876c2395597f181dc98c6dc
edermis <edermis@hotmail.com>
2023-06-03 09:30:47 -0400

    refactor custom method

--- src/Edermis/Week1/OddOrEven.java
+++ src/Edermis/Week1/OddOrEven.java
@@ -10,10 +10,10 @@ identify(6); ->"Even"
 public class OddOrEven {
     public static void main(String[] args) {
 
-        OddOrEven(5);//odd
-        OddOrEven(6);//even
+        oddOrEven(5);//odd
+        oddOrEven(6);//even
     }
-    public static void OddOrEven(int number){
+    public static void oddOrEven(int number){
 
         if (number % 2 == 0 && number %2 !=1) {
 
