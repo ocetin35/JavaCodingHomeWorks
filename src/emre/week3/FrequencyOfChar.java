0afb667fcb3459ad8611fe600592c9d4aab4410d
Emre Durmaz <emredrmz@gmail.com>
2023-06-06 18:32:40 -0400

    weeks 3 codes added.

---
+++ src/emre/week3/FrequencyOfChar.java
@@ -0,0 +1,9 @@
+package emre.week3;
+
+public class FrequencyOfChar {
+}
+
+
+//      3) String - Frequency of Characters
+//      Write a return method that can find the frequency of characters
+//      Ex:  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
---
+++ src/emre/week3/ReverseNegativeNumbers.java
@@ -0,0 +1,10 @@
+package emre.week3;
+
+public class ReverseNegativeNumbers {
+
+
+}
+
+
+//   2) Number - Reverse negative number
+//   Write a return method that can reverse negative number and return it as int
