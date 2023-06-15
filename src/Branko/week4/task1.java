0a3f4bdafa38f300bd92d8cf068aa0aada3911b8
Branko <banebumbar88@hotmail.com>
2023-06-15 18:19:06 -0400

    Week4

---
+++ src/Branko/week4/task1.java
@@ -0,0 +1,37 @@
+package Branko.week4;
+
+public class task1 {
+    //1) String - Same letters
+    //Write a return method that check if a string is build out of the same letters as another string.
+    //Ex:  same("abc",  "cab"); -> true 
+    //same("abc",  "abb"); -> false:
+    public static boolean same(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        int[] counts = new int[26];
+
+
+        for (int i = 0; i < str1.length(); i++) {
+            char c = str1.charAt(i);
+            counts[c - 'a']++;
+        }
+
+
+        for (int i = 0; i < str2.length(); i++) {
+            char c = str2.charAt(i);
+            counts[c - 'a']--;
+        }
+
+
+        for (int count : counts) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
